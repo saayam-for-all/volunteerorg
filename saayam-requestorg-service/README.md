ddf8464706896ca4cde52a32dc6effa574ccb0d4
adi0546 <adityanarang010@gmail.com>
2024-06-20 22:42:05 -0400

    Initialised VolunteerOrg repo with Spring3 Java17

---
+++ saayam-requestorg-service/README.md
@@ -0,0 +1,1 @@
+# requestorg service readme
---
+++ saayam-requestorg-service/src/main/java/com/saayamvolunteerorgservice/saayam_volunteerorg_service/SaayamVolunteerOrgServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saayamvolunteerorgservice.saayam_volunteerorg_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaayamVolunteerOrgServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaayamVolunteerOrgServiceApplication.class, args);
+	}
+
+}
